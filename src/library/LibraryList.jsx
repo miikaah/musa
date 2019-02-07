5385b509d93fa1006c4fb28d4e8a208984958eef
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-02-07 17:43:19 +0200

    chore: let prettier add semicolons after all

--- src/library/LibraryList.jsx
+++ src/library/LibraryList.jsx
@@ -1,46 +1,45 @@
-import React, { Component } from "react"
-import LibraryItem from "./LibraryItem"
-import "./LibraryList.scss"
+import React, { Component } from "react";
+import LibraryItem from "./LibraryItem";
+import "./LibraryList.scss";
 
 class LibraryList extends Component {
   state = {
     showFolderItems: false
-  }
+  };
 
   render() {
-    const item = this.props.item
+    const item = this.props.item;
     if (Array.isArray(item.children)) {
       return (
         <ul className="library-list">
           <li
             className="library-list-folder"
             key={item.name}
-            onClick={this.onClick.bind(this)}>
-              {item.name}
+            onClick={this.toggleFolder.bind(this)}
+          >
+            {item.name}
           </li>
-          {
-            this.state.showFolderItems && item.children
-              .map(child =>
-                <LibraryList
-                  key={child.name.toString() + "-" + Date.now()}
-                  item={child}
-                />
-              )
-          }
+          {this.state.showFolderItems &&
+            item.children.map(child => (
+              <LibraryList
+                key={child.name.toString() + "-" + Date.now()}
+                item={child}
+              />
+            ))}
         </ul>
-      )
+      );
     }
     return (
       <LibraryItem key={item.name.toString() + "-" + Date.now()} item={item} />
-    )
+    );
   }
 
-  onClick(event) {
-    event.preventDefault()
+  toggleFolder(event) {
+    event.preventDefault();
     this.setState({
       showFolderItems: !this.state.showFolderItems
-    })
+    });
   }
 }
 
-export default LibraryList
+export default LibraryList;
