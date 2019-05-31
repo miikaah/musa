0e5ec2e890842501d7b1c575a6532d1f1bd31507
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-05-31 14:58:30 +0300

    refactor: Library to function component

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import LibraryList from "./LibraryList"
 import { isEqual, isEmpty, get, flatten } from "lodash-es"
 import { connect } from "react-redux"
@@ -13,122 +13,123 @@ const dbVersion = 3
 
 const idbRequest = indexedDB.open(dbName, dbVersion)
 
-class Library extends Component {
-  state = {
-    listing: []
-  }
+let isInit = false
 
-  componentDidMount() {
-    ipcRenderer.on("log", (event, log) => console.log("(main)", log))
-    ipcRenderer.on("error", (event, error) => console.error(error))
-    idbRequest.onerror = event => console.error(event)
-    idbRequest.onupgradeneeded = event => {
-      try {
-        event.target.result.createObjectStore("songList", { keyPath: "key" })
-      } catch (e) {
-        console.log(e)
-      }
-      try {
-        event.target.result.createObjectStore("library", { keyPath: "path" })
-      } catch (e) {
-        console.log(e)
-      }
-    }
-    idbRequest.onsuccess = event => {
-      const db = event.target.result
-      const songListStore = db
-        .transaction("songList", "readwrite")
-        .objectStore("songList")
+// https://reactjs.org/docs/hooks-faq.html#how-do-i-implement-shouldcomponentupdate
+const Library = React.memo(
+  ({ isVisible, dispatch }) => {
+    const [listing, setListing] = useState([])
 
-      const songListStoreRequest = songListStore.get("list")
+    if (!isInit) {
+      let dbListing = []
 
-      songListStoreRequest.onsuccess = event => {
-        ipcRenderer.on("updateSongList", (event, songList) => {
-          const songListOS = db
-            .transaction("songList", "readwrite")
-            .objectStore("songList")
-          songListOS.put({ key: "list", list: songList })
-        })
-        const dbSongList = songListStoreRequest.result
-        ipcRenderer.send("initLibrary", get(dbSongList, "list", []))
+      const setNewListing = newRecord => {
+        if (isEmpty(newRecord)) return
+        setListing(
+          [
+            ...dbListing.filter(record => record.path !== newRecord.path),
+            newRecord
+          ].sort((a, b) => a.path.localeCompare(b.path))
+        )
       }
 
-      /////
-      const objectStore = db
-        .transaction("library", "readwrite")
-        .objectStore("library")
+      ipcRenderer.on("log", (event, log) => console.log("(main)", log))
+      ipcRenderer.on("error", (event, error) => console.error(error))
+      idbRequest.onerror = event => console.error(event)
+      idbRequest.onupgradeneeded = event => {
+        try {
+          event.target.result.createObjectStore("songList", { keyPath: "key" })
+        } catch (e) {
+          console.log(e)
+        }
+        try {
+          event.target.result.createObjectStore("library", { keyPath: "path" })
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      idbRequest.onsuccess = event => {
+        const db = event.target.result
+        const songListStore = db
+          .transaction("songList", "readwrite")
+          .objectStore("songList")
 
-      const objectStoreRequest = objectStore.getAll()
+        const songListStoreRequest = songListStore.get("list")
 
-      objectStoreRequest.onsuccess = event => {
-        const dbListing = objectStoreRequest.result
-        this.setState({ listing: dbListing })
-
-        ipcRenderer.on("libraryListing", (event, listing) => {
-          const libraryOS = db
-            .transaction("library", "readwrite")
-            .objectStore("library")
-          this.setState({ listing: this.getNewListing(listing) })
-          libraryOS.put(listing)
-        })
-
-        ipcRenderer.on("deleteLibraryListings", (event, keyPaths) => {
-          const libraryOS = db
-            .transaction("library", "readwrite")
-            .objectStore("library")
-          this.setState({
-            listing: this.state.listing.filter(
-              artist => !keyPaths.includes(artist.path)
-            )
+        songListStoreRequest.onsuccess = event => {
+          ipcRenderer.on("updateSongList", (event, songList) => {
+            const songListOS = db
+              .transaction("songList", "readwrite")
+              .objectStore("songList")
+            songListOS.put({ key: "list", list: songList })
           })
-          keyPaths.forEach(key => libraryOS.delete(key))
-        })
+          const dbSongList = songListStoreRequest.result
+          ipcRenderer.send("initLibrary", get(dbSongList, "list", []))
+        }
 
-        ipcRenderer.on("updateSongMetadata", (event, song) => {
-          const artistOS = db
-            .transaction("library", "readwrite")
-            .objectStore("library")
-          const artistReq = artistOS.get(song.artistPath)
-          artistReq.onsuccess = event => {
-            const artist = artistReq.result
-            const oldSong = flatten(
-              get(artist, "albums", []).map(a => a.songs)
-            ).find(s => s.path === song.path)
-            oldSong.metadata = song.metadata
-            artistOS.put(artist)
-            this.setState({ listing: this.getNewListing(artist) })
-          }
+        /////
+        const objectStore = db
+          .transaction("library", "readwrite")
+          .objectStore("library")
+
+        const objectStoreRequest = objectStore.getAll()
+
+        objectStoreRequest.onsuccess = event => {
+          dbListing = objectStoreRequest.result
+          setListing(dbListing)
+
+          ipcRenderer.on("libraryListing", (event, listing) => {
+            const libraryOS = db
+              .transaction("library", "readwrite")
+              .objectStore("library")
+            setNewListing(listing)
+            libraryOS.put(listing)
+          })
+
+          ipcRenderer.on("deleteLibraryListings", (event, keyPath) => {
+            const libraryOS = db
+              .transaction("library", "readwrite")
+              .objectStore("library")
+            dbListing = dbListing.filter(
+              artist => !keyPath.includes(artist.path)
+            )
+            setListing(dbListing)
+            libraryOS.delete(keyPath)
+          })
+
+          ipcRenderer.on("updateSongMetadata", (event, song) => {
+            const artistOS = db
+              .transaction("library", "readwrite")
+              .objectStore("library")
+            const artistReq = artistOS.get(song.artistPath)
+            artistReq.onsuccess = event => {
+              const artist = artistReq.result
+              const oldSong = flatten(
+                get(artist, "albums", []).map(a => a.songs)
+              ).find(s => s.path === song.path)
+              oldSong.metadata = song.metadata
+              artistOS.put(artist)
+              setNewListing(artist)
+            }
+          })
+        }
+
+        ipcRenderer.on("startInitialScan", (event, scanLength) => {
+          dispatch(setScanProps({ scanLength }))
         })
+        ipcRenderer.on("updateInitialScan", (event, scannedLength) => {
+          dispatch(setScanProps({ scannedLength }))
+        })
+        ipcRenderer.on("endInitialScan", () => {
+          dispatch(setScanProps({ reset: true }))
+        })
+        isInit = true
       }
-
-      ipcRenderer.on("startInitialScan", (event, scanLength) => {
-        this.props.dispatch(setScanProps({ scanLength }))
-      })
-      ipcRenderer.on("updateInitialScan", (event, scannedLength) => {
-        this.props.dispatch(setScanProps({ scannedLength }))
-      })
-      ipcRenderer.on("endInitialScan", () => {
-        this.props.dispatch(setScanProps({ reset: true }))
-      })
     }
-  }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return !isEqual(this.state, nextState) || !isEqual(this.props, nextProps)
-  }
-
-  getNewListing(listing) {
-    if (isEmpty(listing)) return [...this.state.listing]
-    return [
-      ...this.state.listing.filter(artist => artist.path !== listing.path),
-      listing
-    ].sort((a, b) => a.path.localeCompare(b.path))
-  }
-
-  render() {
     return (
-      <div className={`library ${this.props.isVisible ? "show" : "hide"}`}>
-        {this.state.listing.map((item, index) => (
+      <div className={`library ${isVisible ? "show" : "hide"}`}>
+        {listing.map((item, index) => (
           <LibraryList
             key={item.name + "-" + index}
             item={item}
@@ -137,8 +138,9 @@ class Library extends Component {
         ))}
       </div>
     )
-  }
-}
+  },
+  (props, nextProps) => !isEqual(props, nextProps)
+)
 
 export default connect(
   state => ({
