d382a7d00a3600d39ca02b475f54c4682b706f45
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-02 12:08:47 +0200

    refactor: AlbumCover to a styled component

--- src/components/AlbumCover.jsx
+++ src/components/AlbumCover.jsx
@@ -1,10 +1,45 @@
-import React from "react"
-import { isEmpty } from "lodash-es"
-import { encodeFileUri } from "../util"
-import "./AlbumCover.scss"
+import React from "react";
+import { isEmpty } from "lodash-es";
+import styled from "styled-components/macro";
+import { encodeFileUri } from "../util";
+
+const AlbumCoverContainer = styled.div`
+  display: flex;
+  flex: 30%;
+  margin: 0 24px 30px 0;
+  max-width: 30%;
+  position: relative;
+  cursor: pointer;
+  border: 2px solid transparent;
+  border-top-width: 0;
+  border-right-width: 0;
+  border-left-width: 0;
+
+  > img {
+    flex: 20%;
+    min-width: 60px;
+    max-width: 60px;
+    min-height: 60px;
+    max-height: 60px;
+    background-color: #000;
+  }
+
+  > div {
+    font-size: var(--font-size-sm);
+    padding: 0 25px 0 20px;
+
+    > p {
+      margin: 0 0 8px;
+    }
+  }
+
+  &:hover {
+    border-color: var(--color-primary-highlight);
+  }
+`;
 
 const AlbumCover = ({ item, onClick }) => (
-  <div className="album-cover" onClick={onClick}>
+  <AlbumCoverContainer onClick={onClick}>
     <img
       alt=""
       src={isEmpty(item.cover) ? "" : encodeFileUri(`file://${item.cover}`)}
@@ -13,7 +48,7 @@ const AlbumCover = ({ item, onClick }) => (
       <p>{item.name}</p>
       <p>{item.date}</p>
     </div>
-  </div>
-)
+  </AlbumCoverContainer>
+);
 
-export default AlbumCover
+export default AlbumCover;
