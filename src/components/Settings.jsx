d3a92c5d3981f491e9df2a680964bc618fff5fa4
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-02 16:09:12 +0200

    refactor: Settings to a styled component

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components/macro";
 import ThemeLibrary from "./ThemeLibrary";
 import ReplaygainSetting from "./ReplaygainSetting";
 import MusicLibrarySetting from "./MusicLibrarySetting";
 import Button from "./Button";
-import "./Settings.scss";
+
+const SettingsBlock = styled.div`
+  margin-bottom: 60px;
+`;
 
 const electron = window.require("electron");
 const ipcRenderer = electron.ipcRenderer;
@@ -17,24 +21,24 @@ const Settings = ({ isVisible, musicLibraryPaths }) => {
   return (
     <>
       <h1>Settings</h1>
-      <div className="settings-block">
+      <SettingsBlock>
         <h3>Library</h3>
         <MusicLibrarySetting />
-      </div>
-      <div className="settings-block">
+      </SettingsBlock>
+      <SettingsBlock>
         <h3>Theme</h3>
         <ThemeLibrary update={isVisible} />
-      </div>
-      <div className="settings-block">
+      </SettingsBlock>
+      <SettingsBlock>
         <h3>Replaygain</h3>
         <ReplaygainSetting />
-      </div>
-      <div className="settings-block">
+      </SettingsBlock>
+      <SettingsBlock>
         <h3>Advanced</h3>
         <Button onClick={runInitialScan} isPrimary>
           Re-run initial scan
         </Button>
-      </div>
+      </SettingsBlock>
     </>
   );
 };
