e65e976249cf23d55950a9f6dc57bde798296010
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-02 12:23:06 +0200

    refactor: Artist to a styled-component

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import { isEmpty } from "lodash-es";
+import styled from "styled-components/macro";
 import { dispatchToast } from "../util";
 import { pasteToPlaylist } from "reducers/player.reducer";
 import AlbumCover from "./AlbumCover";
-import "./Artist.scss";
+
+const ArtistContainer = styled.div`
+  flex: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 40px;
+`;
+
+const ArtistName = styled.div`
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const ArtistAlbumList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  flex: 50%;
+`;
 
 const Artist = ({ item, dispatch }) => {
   if (isEmpty(item)) return null;
@@ -23,9 +42,9 @@ const Artist = ({ item, dispatch }) => {
     dispatchToast(msg, key, dispatch);
   };
   return (
-    <div className="artist">
-      <div>{item.name}</div>
-      <div className="artist-album-list">
+    <ArtistContainer>
+      <ArtistName>{item.name}</ArtistName>
+      <ArtistAlbumList>
         {item.albums
           .filter(a => a.name !== "undefined")
           .map((a, i) => (
@@ -35,8 +54,8 @@ const Artist = ({ item, dispatch }) => {
               onClick={() => addAlbumSongsToPlaylist(a)}
             />
           ))}
-      </div>
-    </div>
+      </ArtistAlbumList>
+    </ArtistContainer>
   );
 };
 
