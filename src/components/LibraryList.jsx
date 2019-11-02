03d0dec665fed72c3178dc5e4b55ba3b1fb1a759
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-02 13:02:21 +0200

    refactor: LibraryList to a styled component

--- src/components/LibraryList.jsx
+++ src/components/LibraryList.jsx
@@ -1,20 +1,53 @@
-import React, { useState } from "react"
-import { connect } from "react-redux"
-import { flatten, defaultTo } from "lodash-es"
-import LibraryItem from "./LibraryItem"
-import AlbumCover from "./AlbumCover"
-import "./LibraryList.scss"
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { flatten, defaultTo } from "lodash-es";
+import styled from "styled-components/macro";
+import LibraryItem from "./LibraryItem";
+import AlbumCover from "./AlbumCover";
+
+const LibraryListContainer = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  text-align: left;
+  padding-left: ${({ isRoot }) => (isRoot ? 0 : 12)}px;
+  font-size: 0.9rem;
+
+  > li:nth-child(2) {
+    padding-top: 4px;
+  }
+
+  > li:last-child:not(:first-child) {
+    margin-bottom: 20px;
+  }
+
+  > li:hover {
+    background-color: var(--color-secondary-highlight);
+    color: var(--color-typography-secondary);
+  }
+`;
+
+const LibraryListFolder = styled.li`
+  cursor: pointer;
+  padding: 2px 12px;
+
+  > div {
+    max-width: 100%;
+    border: 0;
+    margin: 5px 0;
+  }
+`;
 
 const LibraryList = ({ item, cover, isRoot, dispatch }) => {
-  const [showFolderItems, setShowFolderItems] = useState(false)
-  const isArtist = Array.isArray(item.albums)
-  const isAlbum = Array.isArray(item.songs)
-  const isUndefinedItemName = item.name === "undefined"
+  const [showFolderItems, setShowFolderItems] = useState(false);
+  const isArtist = Array.isArray(item.albums);
+  const isAlbum = Array.isArray(item.songs);
+  const isUndefinedItemName = item.name === "undefined";
 
   const toggleFolder = event => {
-    event.preventDefault()
-    setShowFolderItems(!showFolderItems)
-  }
+    event.preventDefault();
+    setShowFolderItems(!showFolderItems);
+  };
 
   const getArtistOrAlbumSongs = () => {
     if (isArtist)
@@ -22,39 +55,32 @@ const LibraryList = ({ item, cover, isRoot, dispatch }) => {
         item.albums.map(a =>
           defaultTo(a.songs, []).map(s => ({ ...s, cover: a.cover }))
         )
-      )
-    if (isAlbum) return item.songs.map(song => ({ ...song, cover: item.cover }))
-  }
+      );
+    if (isAlbum)
+      return item.songs.map(song => ({ ...song, cover: item.cover }));
+  };
 
   const onDragStart = event => {
     event.dataTransfer.setData(
       "text/plain",
       JSON.stringify(getArtistOrAlbumSongs())
-    )
-    event.stopPropagation()
-  }
+    );
+    event.stopPropagation();
+  };
 
   const renderItemsWithoutAlbum = () =>
     item.songs.filter(Boolean).map((song, i) => {
-      return <LibraryItem key={`${song.name}-${i}`} item={song} />
-    })
+      return <LibraryItem key={`${song.name}-${i}`} item={song} />;
+    });
 
   const renderFolderName = () =>
-    isAlbum ? <AlbumCover item={item} /> : item.name
+    isAlbum ? <AlbumCover item={item} /> : item.name;
 
   const renderArtistsAndAlbums = () => (
-    <ul
-      className={`library-list ${isRoot ? "root" : ""}`}
-      draggable
-      onDragStart={onDragStart}
-    >
-      <li
-        className="library-list-folder"
-        key={item.name}
-        onClick={toggleFolder}
-      >
+    <LibraryListContainer isRoot={isRoot} draggable onDragStart={onDragStart}>
+      <LibraryListFolder key={item.name} onClick={toggleFolder}>
         {renderFolderName()}
-      </li>
+      </LibraryListFolder>
       {showFolderItems &&
         (item.albums || item.songs).map((child, i) => (
           <LibraryList
@@ -64,18 +90,18 @@ const LibraryList = ({ item, cover, isRoot, dispatch }) => {
             dispatch={dispatch}
           />
         ))}
-    </ul>
-  )
+    </LibraryListContainer>
+  );
 
   if (isArtist || isAlbum) {
     return isUndefinedItemName
       ? renderItemsWithoutAlbum()
-      : renderArtistsAndAlbums()
+      : renderArtistsAndAlbums();
   }
-  return <LibraryItem item={item} cover={cover} hasAlbum />
-}
+  return <LibraryItem item={item} cover={cover} hasAlbum />;
+};
 
 export default connect(
   () => ({}),
   dispatch => ({ dispatch })
-)(LibraryList)
+)(LibraryList);
