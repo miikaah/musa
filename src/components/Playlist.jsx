0cb2aaea98058f8506fb096a07e4e8f6281c5009
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-02 15:49:21 +0200

    refactor: Playlist to use useKeyPress hook

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -12,6 +12,7 @@ import {
 import { KEYS } from "../util";
 import { breakpoint } from "../breakpoints";
 import { Cell } from "../common.styles";
+import { useKeyPress } from "../hooks";
 import PlaylistItem from "./PlaylistItem";
 
 const PlaylistContainer = styled.ul`
@@ -54,266 +55,227 @@ const Playlist = ({
   const [selectedIndexes, setSelectedIndexes] = useState(new Set());
   const [clipboard, setClipboard] = useState([]);
 
-  useEffect(() => {
-    const isCtrlDown = event => event.ctrlKey || event.metaKey;
+  const isCtrlDown = event => event.ctrlKey || event.metaKey;
 
-    const isContinuousSelection = () => {
-      return (
-        !isNaN(startIndex) &&
-        !isNaN(endIndex) &&
-        startIndex !== endIndex &&
-        selectedIndexes.size < 2
-      );
-    };
+  const isContinuousSelection = () => {
+    return (
+      !isNaN(startIndex) &&
+      !isNaN(endIndex) &&
+      startIndex !== endIndex &&
+      selectedIndexes.size < 2
+    );
+  };
 
-    const getContinuousSelData = getSelected => {
-      const sIndex = Math.min(startIndex, endIndex);
-      const eIndex = Math.max(startIndex, endIndex);
-      const selectedItems = getSelected
-        ? playlist.filter((_, index) => index >= sIndex && index <= eIndex)
-        : clipboard;
-      return { startIndex: sIndex, endIndex: eIndex, selectedItems };
-    };
+  const getContinuousSelData = getSelected => {
+    const sIndex = Math.min(startIndex, endIndex);
+    const eIndex = Math.max(startIndex, endIndex);
+    const selectedItems = getSelected
+      ? playlist.filter((_, index) => index >= sIndex && index <= eIndex)
+      : clipboard;
+    return { startIndex: sIndex, endIndex: eIndex, selectedItems };
+  };
 
-    const handleContinuousSelection = ({ type, getSelected = true }) => {
-      const { startIndex, endIndex, selectedItems } = getContinuousSelData(
-        getSelected
-      );
+  const handleContinuousSelection = ({ type, getSelected = true }) => {
+    const { startIndex, endIndex, selectedItems } = getContinuousSelData(
+      getSelected
+    );
 
-      switch (type) {
-        case "remove": {
-          dispatch(removeRangeFromPlaylist(startIndex, endIndex));
-          setSelectedIndexes(new Set());
-          setStartIndex(NaN);
-          setEndIndex(NaN);
-          setClipboard(selectedItems);
-          setActiveIndex(-1);
-          return;
-        }
-
-        case "duplicate": {
-          dispatch(pasteToPlaylist(selectedItems, activeIndex));
-          return;
-        }
-
-        default: {
-          setClipboard(selectedItems);
-        }
-      }
-    };
-
-    const isIndexesSelection = () => {
-      return activeIndex > -1 || selectedIndexes.size > 0;
-    };
-
-    const getIndexesSelData = getSelected => {
-      const indexes =
-        selectedIndexes.size > 0
-          ? Array.from(selectedIndexes.values())
-          : [activeIndex];
-      const selectedItems = getSelected
-        ? indexes.map(i => playlist[i])
-        : clipboard;
-      return { indexes, selectedItems };
-    };
-
-    const handleIndexesSelection = ({ type, getSelected = true }) => {
-      const { indexes, selectedItems } = getIndexesSelData(getSelected);
-
-      switch (type) {
-        case "remove": {
-          dispatch(removeIndexesFromPlaylist(indexes));
-          setSelectedIndexes(new Set());
-          setStartIndex(NaN);
-          setEndIndex(NaN);
-          setClipboard(selectedItems);
-          setActiveIndex(selectedIndexes.size > 1 ? -1 : activeIndex);
-          return;
-        }
-
-        case "duplicate": {
-          dispatch(pasteToPlaylist(selectedItems, activeIndex));
-          return;
-        }
-
-        default: {
-          setClipboard(selectedItems);
-        }
-      }
-    };
-
-    const shouldReplaySong = () => {
-      return (
-        activeIndex === currentIndex &&
-        isEqual(currentItem, playlist[activeIndex])
-      );
-    };
-
-    const handleKeyDown = event => {
-      if (get(event, "target.tagName") !== "BODY") return;
-
-      const setNewIndexes = newActiveIndex => {
-        if (event.shiftKey) {
-          setStartIndex(!isNaN(startIndex) ? startIndex : activeIndex);
-          setEndIndex(newActiveIndex);
-          setActiveIndex(newActiveIndex);
-          return;
-        }
+    switch (type) {
+      case "remove": {
+        dispatch(removeRangeFromPlaylist(startIndex, endIndex));
+        setSelectedIndexes(new Set());
         setStartIndex(NaN);
         setEndIndex(NaN);
-        setActiveIndex(newActiveIndex);
-        setSelectedIndexes(new Set());
-      };
-
-      switch (event.keyCode) {
-        // SELECT ALL
-        case KEYS.A: {
-          if (!isCtrlDown(event)) return;
-          setStartIndex(0);
-          setEndIndex(playlist.length - 1);
-          return;
-        }
-
-        // MOVE UP
-        case KEYS.Up: {
-          event.preventDefault();
-          if (activeIndex - 1 > -1) return setNewIndexes(activeIndex - 1);
-          if (playlist.length) return setActiveIndex(playlist.length - 1);
-          return;
-        }
-
-        // MOVE DOWN
-        case KEYS.Down: {
-          event.preventDefault();
-          if (activeIndex + 1 < playlist.length)
-            return setNewIndexes(activeIndex + 1);
-          if (playlist.length) return setActiveIndex(0);
-          return;
-        }
-
-        case KEYS.Enter: {
-          // CREATE NEW PLAYLIST FROM SELECTION
-          if (isContinuousSelection() || selectedIndexes.size > 1) {
-            let selItems;
-            if (isContinuousSelection()) {
-              const { selectedItems } = getContinuousSelData(true);
-              selItems = selectedItems;
-            }
-            if (selectedIndexes.size > 1) {
-              const { selectedItems } = getIndexesSelData(true);
-              selItems = selectedItems;
-            }
-
-            dispatch(removeRangeFromPlaylist(0, playlist.length - 1));
-            dispatch(pasteToPlaylist(selItems, 0));
-            setStartIndex(NaN);
-            setEndIndex(NaN);
-            setActiveIndex(0);
-            return;
-          }
-
-          // PLAY OR REPLAY ACTIVE ITEM
-          if (activeIndex < 0) return;
-          if (shouldReplaySong()) {
-            dispatch(replay(true));
-            return;
-          }
-          dispatch(playIndex(activeIndex));
-          return;
-        }
-
-        // REMOVE
-        case KEYS.Backspace:
-        case KEYS.Delete: {
-          if (isContinuousSelection()) {
-            handleContinuousSelection({
-              type: "remove",
-              getSelected: false
-            });
-            return;
-          }
-          if (isIndexesSelection()) {
-            handleIndexesSelection({ type: "remove", getSelected: false });
-            return;
-          }
-          return;
-        }
-
-        // CUT
-        case KEYS.X: {
-          if (!isCtrlDown(event)) return;
-          if (isContinuousSelection()) {
-            handleContinuousSelection({ type: "remove" });
-            return;
-          }
-          if (isIndexesSelection()) {
-            handleIndexesSelection({ type: "remove" });
-            return;
-          }
-          return;
-        }
-
-        // COPY
-        case KEYS.C: {
-          if (!isCtrlDown(event)) return;
-          if (isContinuousSelection()) {
-            handleContinuousSelection({ type: "copy" });
-            return;
-          }
-          if (isIndexesSelection()) {
-            handleIndexesSelection({ type: "copy" });
-            return;
-          }
-          return;
-        }
-
-        // DUPLICATE
-        case KEYS.D: {
-          if (!isCtrlDown(event) || !event.shiftKey) return;
-          if (isContinuousSelection()) {
-            handleContinuousSelection({ type: "duplicate" });
-            return;
-          }
-          if (isIndexesSelection()) {
-            handleIndexesSelection({ type: "duplicate" });
-            return;
-          }
-          return;
-        }
-
-        // PASTE
-        case KEYS.V: {
-          if (!isCtrlDown(event)) return;
-          dispatch(
-            pasteToPlaylist(
-              clipboard,
-              event.shiftKey ? activeIndex - 1 : activeIndex
-            )
-          );
-          return;
-        }
-
-        default:
-          break;
+        setClipboard(selectedItems);
+        setActiveIndex(-1);
+        return;
       }
-    };
 
-    window.addEventListener("keydown", handleKeyDown);
+      case "duplicate": {
+        dispatch(pasteToPlaylist(selectedItems, activeIndex));
+        return;
+      }
 
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    activeIndex,
-    startIndex,
-    endIndex,
-    currentItem,
-    currentIndex,
-    selectedIndexes,
-    clipboard,
-    playlist
-  ]);
+      default: {
+        setClipboard(selectedItems);
+      }
+    }
+  };
+
+  const isIndexesSelection = () => {
+    return activeIndex > -1 || selectedIndexes.size > 0;
+  };
+
+  const getIndexesSelData = getSelected => {
+    const indexes =
+      selectedIndexes.size > 0
+        ? Array.from(selectedIndexes.values())
+        : [activeIndex];
+    const selectedItems = getSelected
+      ? indexes.map(i => playlist[i])
+      : clipboard;
+    return { indexes, selectedItems };
+  };
+
+  const handleIndexesSelection = ({ type, getSelected = true }) => {
+    const { indexes, selectedItems } = getIndexesSelData(getSelected);
+
+    switch (type) {
+      case "remove": {
+        dispatch(removeIndexesFromPlaylist(indexes));
+        setSelectedIndexes(new Set());
+        setStartIndex(NaN);
+        setEndIndex(NaN);
+        setClipboard(selectedItems);
+        setActiveIndex(selectedIndexes.size > 1 ? -1 : activeIndex);
+        return;
+      }
+
+      case "duplicate": {
+        dispatch(pasteToPlaylist(selectedItems, activeIndex));
+        return;
+      }
+
+      default: {
+        setClipboard(selectedItems);
+      }
+    }
+  };
+
+  const shouldReplaySong = () => {
+    return (
+      activeIndex === currentIndex &&
+      isEqual(currentItem, playlist[activeIndex])
+    );
+  };
+
+  const setNewIndexes = (event, newActiveIndex) => {
+    if (event.shiftKey) {
+      setStartIndex(!isNaN(startIndex) ? startIndex : activeIndex);
+      setEndIndex(newActiveIndex);
+      setActiveIndex(newActiveIndex);
+      return;
+    }
+    setStartIndex(NaN);
+    setEndIndex(NaN);
+    setActiveIndex(newActiveIndex);
+    setSelectedIndexes(new Set());
+  };
+
+  const selectAll = event => {
+    if (!isCtrlDown(event)) return;
+    setStartIndex(0);
+    setEndIndex(playlist.length - 1);
+  };
+  useKeyPress(KEYS.A, selectAll);
+
+  const moveUp = event => {
+    event.preventDefault();
+    if (activeIndex - 1 > -1) return setNewIndexes(event, activeIndex - 1);
+    if (playlist.length) return setActiveIndex(playlist.length - 1);
+  };
+  useKeyPress(KEYS.Up, moveUp);
+
+  const moveDown = event => {
+    event.preventDefault();
+    if (activeIndex + 1 < playlist.length)
+      return setNewIndexes(event, activeIndex + 1);
+    if (playlist.length) return setActiveIndex(0);
+  };
+  useKeyPress(KEYS.Down, moveDown);
+
+  const createNewPlaylistFromSelection = () => {
+    if (isContinuousSelection() || selectedIndexes.size > 1) {
+      let selItems;
+      if (isContinuousSelection()) {
+        const { selectedItems } = getContinuousSelData(true);
+        selItems = selectedItems;
+      }
+      if (selectedIndexes.size > 1) {
+        const { selectedItems } = getIndexesSelData(true);
+        selItems = selectedItems;
+      }
+
+      dispatch(removeRangeFromPlaylist(0, playlist.length - 1));
+      dispatch(pasteToPlaylist(selItems, 0));
+      setStartIndex(NaN);
+      setEndIndex(NaN);
+      setActiveIndex(0);
+      return;
+    }
+
+    // PLAY OR REPLAY ACTIVE ITEM
+    if (activeIndex < 0) return;
+    if (shouldReplaySong()) {
+      dispatch(replay(true));
+      return;
+    }
+    dispatch(playIndex(activeIndex));
+  };
+  useKeyPress(KEYS.Enter, createNewPlaylistFromSelection);
+
+  const removeItems = () => {
+    if (isContinuousSelection()) {
+      handleContinuousSelection({
+        type: "remove",
+        getSelected: false
+      });
+      return;
+    }
+    if (isIndexesSelection()) {
+      handleIndexesSelection({ type: "remove", getSelected: false });
+      return;
+    }
+  };
+  useKeyPress(KEYS.Backspace, removeItems);
+  useKeyPress(KEYS.Delete, removeItems);
+
+  const cut = event => {
+    if (!isCtrlDown(event)) return;
+    if (isContinuousSelection()) {
+      handleContinuousSelection({ type: "remove" });
+      return;
+    }
+    if (isIndexesSelection()) {
+      handleIndexesSelection({ type: "remove" });
+      return;
+    }
+  };
+  useKeyPress(KEYS.X, cut);
+
+  const copy = event => {
+    if (!isCtrlDown(event)) return;
+    if (isContinuousSelection()) {
+      handleContinuousSelection({ type: "copy" });
+      return;
+    }
+    if (isIndexesSelection()) {
+      handleIndexesSelection({ type: "copy" });
+      return;
+    }
+  };
+  useKeyPress(KEYS.C, copy);
+
+  const duplicate = event => {
+    if (!isCtrlDown(event) || !event.shiftKey) return;
+    if (isContinuousSelection()) {
+      handleContinuousSelection({ type: "duplicate" });
+      return;
+    }
+    if (isIndexesSelection()) {
+      handleIndexesSelection({ type: "duplicate" });
+      return;
+    }
+  };
+  useKeyPress(KEYS.D, duplicate);
+
+  const paste = event => {
+    if (!isCtrlDown(event)) return;
+    dispatch(
+      pasteToPlaylist(clipboard, event.shiftKey ? activeIndex - 1 : activeIndex)
+    );
+  };
+  useKeyPress(KEYS.V, paste);
 
   const updateEndIndex = endIndex => {
     if (!isMouseDown) return;
