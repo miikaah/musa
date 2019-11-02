269e0712b61c13195016266a29b3a4ab633f7d03
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-02 15:58:47 +0200

    refactor: ProgressBar to a styled component

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,17 +1,31 @@
-import React from "react"
-import { connect } from "react-redux"
-import "./ProgressBar.scss"
+import React from "react";
+import { connect } from "react-redux";
+import styled from "styled-components/macro";
+
+const ProgressBarContainer = styled.div`
+  position: fixed;
+  top: 0;
+  text-align: center;
+  width: 100%;
+  font-size: var(--font-size-sm);
+`;
+
+const ProgressBarValue = styled.div`
+  background-color: var(--color-primary-highlight);
+  width: ${({ width }) => width}%;
+  height: 4px;
+`;
 
 const ProgressBar = ({ scanLength, scannedLength }) => {
-  const width = scanLength > 0 ? (scannedLength / scanLength) * 100 : 0
+  const width = scanLength > 0 ? (scannedLength / scanLength) * 100 : 0;
   return (
-    <div className="progress-bar">
-      <div className="progress-bar-value" style={{ width: `${width}%` }} />
-    </div>
-  )
-}
+    <ProgressBarContainer>
+      <ProgressBarValue width={width} />
+    </ProgressBarContainer>
+  );
+};
 
 export default connect(state => ({
   scanLength: state.library.scanLength,
   scannedLength: state.library.scannedLength
-}))(ProgressBar)
+}))(ProgressBar);
