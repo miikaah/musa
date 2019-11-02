353922dd7b1213c59e6d1aeb90d610e59e37282f
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-02 16:01:43 +0200

    refactor: ReplaygainSetting to a styled component

--- src/components/ReplaygainSetting.jsx
+++ src/components/ReplaygainSetting.jsx
@@ -1,9 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import { get } from "lodash-es";
+import styled from "styled-components/macro";
 import { REPLAYGAIN_TYPE, getStateFromIdb, updateStateInIdb } from "../util";
 import { updateSettings } from "reducers/settings.reducer";
-import { get } from "lodash-es";
-import "./ReplaygainSetting.scss";
+
+const ReplaygainSettingContainer = styled.div`
+  position: relative;
+
+  select {
+    appearance: none;
+    width: 200px;
+    padding: 10px;
+    font-size: var(--font-size-sm);
+    border-radius: 0;
+    background-color: var(--color-primary-highlight);
+    color: var(--color-typography-primary);
+    cursor: pointer;
+  }
+`;
+
+const ArrowDown = styled.span`
+  position: absolute;
+  cursor: pointer;
+  width: 0;
+  height: 0;
+  left: 170px;
+  top: 15px;
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+  border-top: 10px solid var(--color-typography-primary);
+`;
 
 const ReplaygainSetting = ({ replaygainType, dispatch }) => {
   const [type, setType] = useState();
@@ -25,14 +52,14 @@ const ReplaygainSetting = ({ replaygainType, dispatch }) => {
   };
 
   return (
-    <div className="replaygain-setting">
+    <ReplaygainSettingContainer>
       <select value={type} onChange={updateState}>
         <option value={REPLAYGAIN_TYPE.Track}>Track</option>
         <option value={REPLAYGAIN_TYPE.Album}>Album</option>
         <option value={REPLAYGAIN_TYPE.Off}>Off</option>
       </select>
-      <span className="arrow-down" />
-    </div>
+      <ArrowDown />
+    </ReplaygainSettingContainer>
   );
 };
 
