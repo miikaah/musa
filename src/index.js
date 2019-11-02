857960e065029c633b387362cbf29c488f974b5c
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-02 11:31:54 +0200

    chore: change index.scss to index.css

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
-import { createStore } from "redux"
-import rootReducer from "./reducers"
-import "./index.scss"
-import App from "./App.jsx"
-import * as serviceWorker from "./serviceWorker"
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import "./index.css";
+import App from "./App.jsx";
+import * as serviceWorker from "./serviceWorker";
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
