e4101a7eca933d35cd4f88a3ddd16f2213da7895
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-11-09 15:21:22 +0200

    Move IDB state update from settings.reducer to index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 import App from "./App.jsx";
+import { getStateFromIdb, updateStateInIdb } from "./util";
 import "./index.css";
 
 export const store = createStore(rootReducer);
+
+store.subscribe(() => {
+  getStateFromIdb((req, db) => () =>
+    updateStateInIdb(req, db, store.getState().settings)
+  );
+});
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/settings.reducer.js
+++ src/reducers/settings.reducer.js
@@ -1,13 +1,13 @@
-import { FALLBACK_THEME } from "../config"
-import { REPLAYGAIN_TYPE, getStateFromIdb, updateStateInIdb } from "../util"
+import { FALLBACK_THEME } from "../config";
+import { REPLAYGAIN_TYPE } from "../util";
 
-export const UPDATE_SETTINGS = "MUSA/SETTINGS/UPDATE_SETTINGS"
+export const UPDATE_SETTINGS = "MUSA/SETTINGS/UPDATE_SETTINGS";
 export const updateSettings = props => ({
   type: UPDATE_SETTINGS,
   props
-})
+});
 
-export const VOLUME_DEFAULT = 50
+export const VOLUME_DEFAULT = 50;
 
 const initialState = {
   currentTheme: FALLBACK_THEME,
@@ -16,21 +16,19 @@ const initialState = {
   replaygainType: REPLAYGAIN_TYPE.Track,
   volume: VOLUME_DEFAULT,
   musicLibraryPaths: []
-}
+};
 
 const settings = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_SETTINGS: {
-      const newState = {
+      return {
         ...state,
         ...action.props
-      }
-      getStateFromIdb((req, db) => () => updateStateInIdb(req, db, newState))
-      return newState
+      };
     }
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default settings
+export default settings;
