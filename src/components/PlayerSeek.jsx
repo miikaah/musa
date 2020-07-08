23621c5e68146916192b6a717aedf4042f582948
Miika Henttonen <miika.henttonen@wunderdog.fi>
2020-07-08 16:49:18 +0300

    Update PlayerSeek to a custom implementation instead of a range input

--- src/components/PlayerSeek.jsx
+++ src/components/PlayerSeek.jsx
@@ -1,17 +1,53 @@
 import React, { useState, useRef } from "react";
 import { connect } from "react-redux";
 import { get } from "lodash-es";
-import styled from "styled-components/macro";
-import { useAnimationFrame } from "../hooks";
-import { rangeInput } from "../common.styles";
+import styled, { css } from "styled-components/macro";
+import { useInterval } from "../hooks";
 
-const SeekContainer = styled.span`
-  ${rangeInput}
+const MAIN_BUTTON_DOWN = 1;
+
+const SeekContainer = styled.div`
+  display: flex;
+  align-items: center;
   height: 20px;
+  cursor: pointer;
 
-  input {
-    min-width: 240px;
+  :hover {
+    > div {
+      > div {
+        > div {
+          background-color: red;
+        }
+      }
+    }
   }
+`;
+
+const SeekCss = css`
+  border-radius: 2px;
+  height: 4px;
+  width: 100%;
+`;
+
+const SeekBg = styled.div`
+  ${SeekCss}
+  min-width: 240px;
+  background-color: var(--color-slider-track);
+  display: flex;
+`;
+
+const SeekFgWrapper = styled.div`
+  ${SeekCss}
+  overflow: hidden;
+`;
+
+const SeekFg = styled.div.attrs(({ progress }) => ({
+  style: {
+    transform: `translateX(-${progress}%)`
+  }
+}))`
+  ${SeekCss}
+  background-color: var(--color-slider);
 `;
 
 const PlayerSeek = ({
@@ -19,47 +55,66 @@ const PlayerSeek = ({
   duration,
   currentTime,
   setCurrentTime,
-  isPlaying
+  isPlaying,
+  currentItem
 }) => {
   const [prevCurrentTime, setPrevCurrentTime] = useState(0);
 
   const playerSeek = useRef(null);
 
-  useAnimationFrame(() => {
+  useInterval(() => {
     if (isPlaying) {
       setCurrentTime(get(player, "current.currentTime", 0));
     }
-  });
+  }, 200);
 
-  const seek = event => {
-    if (prevCurrentTime === event.target.value) return;
-    player.current.currentTime = event.target.value;
-    setCurrentTime(event.target.value);
-    setPrevCurrentTime(event.target.value);
+  const seek = seekPosInSeconds => {
+    if (prevCurrentTime === seekPosInSeconds) return;
+    player.current.currentTime = seekPosInSeconds;
+    setCurrentTime(seekPosInSeconds);
+    setPrevCurrentTime(seekPosInSeconds);
     // Makes it possible to seek back to same spot after timeout
     // to prevent multiple seeks
     setTimeout(() => setPrevCurrentTime(-1), 500);
   };
 
+  const hasCurrentItem = () => !!(currentItem && currentItem.metadata);
+
+  const handleSeek = e => {
+    if (!hasCurrentItem()) return;
+    if (e.type === "mousemove" && e.buttons !== MAIN_BUTTON_DOWN) return;
+    const x = e.clientX;
+    const { left, width } = playerSeek.current.getBoundingClientRect();
+    const pos = (x - left) / width;
+    const seekPosInSeconds = Math.floor(duration * pos);
+    seek(seekPosInSeconds);
+  };
+
+  const convertCurrentTimeToPercentage = () => {
+    if (!hasCurrentItem()) return 100;
+    return (1 - currentTime / duration) * 100;
+  };
+
   return (
-    <SeekContainer>
-      <input
-        type="range"
-        min="0"
-        max={duration}
-        step="1"
-        ref={playerSeek}
-        value={currentTime}
-        onChange={seek}
-        onFocus={() => playerSeek.current.blur()}
-      />
+    <SeekContainer
+      onMouseDown={handleSeek}
+      onMouseMove={handleSeek}
+      ref={playerSeek}
+      onFocus={() => playerSeek.current.blur()}
+    >
+      <SeekBg>
+        <SeekFgWrapper>
+          <SeekFg progress={convertCurrentTimeToPercentage()} />
+        </SeekFgWrapper>
+      </SeekBg>
     </SeekContainer>
   );
 };
 
 export default connect(
   state => ({
-    isPlaying: state.player.isPlaying
+    isPlaying: state.player.isPlaying,
+    currentItem: state.player.currentItem
   }),
   dispatch => ({ dispatch })
 )(PlayerSeek);
--- src/hooks.js
+++ src/hooks.js
@@ -50,3 +50,24 @@ export const useThrottle = (value, wait) => {
 
   return throttledValue;
 };
+
+// See: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
