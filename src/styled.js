9194f03f272792b8798c440176caa67a75df10ef
Miika Henttonen <miika.henttonen@kipina.fi>
2023-07-14 17:25:39 +0300

    Update comments

--- src/styled.js
+++ src/styled.js
@@ -5,7 +5,7 @@ import styled, {
 } from "styled-components/macro";
 import isValidProp from "@emotion/is-prop-valid";
 
-// Use this for most use cases so that props don't passthru to React in which case it logs a warning in the console.
+// Use this so that props don't passthru to React and the DOM in which case it logs a warning in the console.
 // We're doing this because styled-components v6 removed automatic filtering.
 export const styledWithPropFilter = (tagName, filterFn) =>
   styled(tagName).withConfig({ shouldForwardProp: filterFn || isValidProp });
@@ -14,5 +14,5 @@ export const css = cssImport;
 export const keyframes = keyframesImport;
 export const ThemeProvider = ThemeProviderImport;
 
-// Use this for HOCs that need to pass props through
+// Use this by default and for HOCs that need to pass props through
 export default styled;
