518818e8e12da2b3096523c1cc912d68d8ac5a32
Miika Henttonen <miika.henttonen@gmail.com>
2023-10-04 11:00:02 +0300

    Remove redundant http proxy from serve.mjs

--- scripts/serve.mjs
+++ scripts/serve.mjs
@@ -1,9 +1,6 @@
 import * as esbuild from "esbuild";
-import http from "node:http";
 import inlineImage from "esbuild-plugin-inline-image";
 import { buildIndex } from "./buildIndex.mjs";
-
-const isWindows = process.platform === "win32";
 
 const {
   OUTDIR = "",
@@ -39,41 +36,8 @@ let ctx = await esbuild.context({
 
 const host = "localhost";
 const port = Number(process.env.PORT || 3666);
-// NOTE: Windows fails with ENOBUFS when using the same port for esbuild and
-//       the http proxy at the same time. This breaks hot reload but
-//       at least it works.
-const esbuildPort = isWindows ? 3665 : port;
 
 await ctx.watch();
-await ctx.serve({ servedir, host, port: esbuildPort });
-
-http
-  .createServer((req, res) => {
-    const options = {
-      hostname: host,
-      port: esbuildPort,
-      path: req.url,
-      method: req.method,
-      headers: req.headers,
-    };
-
-    // Forward each incoming request to esbuild
-    const proxyReq = http.request(options, (proxyRes) => {
-      // If esbuild returns "not found", send a custom 404 page
-      if (proxyRes.statusCode === 404) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h1>A custom 404 page</h1>");
-        return;
-      }
-
-      // Otherwise, forward the response from esbuild to the client
-      res.writeHead(proxyRes.statusCode, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
-    });
-
-    // Forward the body of the request to esbuild
-    req.pipe(proxyReq, { end: true });
-  })
-  .listen(port);
+await ctx.serve({ servedir, host, port });
 
 console.log(`Serving http://${host}:${port}`);
