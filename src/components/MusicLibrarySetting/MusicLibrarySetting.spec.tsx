c62bbeac4071a893c2d4f2f508681daa528e4216
Miika Henttonen <miika.henttonen@kipina.fi>
2023-11-06 17:46:10 +0200

    Add tests for MusicLibrarySetting component

---
+++ src/components/MusicLibrarySetting/MusicLibrarySetting.spec.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import MusicLibrarySetting from "./MusicLibrarySetting";
+import { translate } from "../../i18n";
+import { render } from "../../../test/render";
+import Api from "../../apiClient";
+
+const mockDispatch = vi.fn();
+vi.mock("react-redux", async () => ({
+  ...(await vi.importActual<Record<string, unknown>>("react-redux")),
+  useDispatch: () => mockDispatch,
+}));
+
+vi.mock("../../apiClient");
+vi.mocked(Api.addMusicLibraryPath).mockResolvedValue("/path");
+
+const t = translate("en");
+const addNewButtonText = String(t("settings.library.addNew"));
+
+const state = {
+  settings: {
+    t,
+    musicLibraryPath: "",
+  },
+};
+
+const state2 = {
+  settings: {
+    t,
+    musicLibraryPath: "/music",
+  },
+};
+
+describe("MusicLibrarySetting", () => {
+  it("renders MusicLibrarySetting component", async () => {
+    render(<MusicLibrarySetting />, state);
+
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+    expect(
+      screen.getByTestId("MusicLibrarySettingRemoveButton"),
+    ).toBeInTheDocument();
+    expect(screen.getByText(addNewButtonText)).toBeInTheDocument();
+  });
+
+  it("disables remove button when music library path is empty", async () => {
+    render(<MusicLibrarySetting />, state);
+
+    expect(
+      (
+        screen.getByTestId(
+          "MusicLibrarySettingRemoveButton",
+        ) as HTMLButtonElement
+      ).disabled,
+    ).toBe(true);
+  });
+
+  it("enables remove button when music library path exists", async () => {
+    render(<MusicLibrarySetting />, state2);
+
+    expect(
+      (
+        screen.getByTestId(
+          "MusicLibrarySettingRemoveButton",
+        ) as HTMLButtonElement
+      ).disabled,
+    ).toBe(false);
+  });
+
+  it("enables add button when music library path is empty", async () => {
+    render(<MusicLibrarySetting />, state);
+
+    expect(
+      (screen.getByText(addNewButtonText) as HTMLButtonElement).disabled,
+    ).toBe(false);
+  });
+
+  it("disables add button when music library path exists", async () => {
+    render(<MusicLibrarySetting />, state2);
+
+    expect(
+      (screen.getByText(addNewButtonText) as HTMLButtonElement).disabled,
+    ).toBe(true);
+  });
+
+  it("dispatches remove music library path action", async () => {
+    render(<MusicLibrarySetting />, state2);
+
+    await userEvent.click(
+      screen.getByTestId("MusicLibrarySettingRemoveButton"),
+    );
+
+    expect(mockDispatch).toHaveBeenCalledWith(
+      expect.objectContaining({ props: { musicLibraryPath: "" } }),
+    );
+  });
+
+  it("calls api and dispatches add music library path action thunk", async () => {
+    render(<MusicLibrarySetting />, state);
+
+    await userEvent.click(screen.getByText(addNewButtonText));
+
+    expect(mockDispatch).toHaveBeenCalledWith(
+      expect.objectContaining({ props: { musicLibraryPath: "/path" } }),
+    );
+  });
+});
--- src/components/MusicLibrarySetting/MusicLibrarySetting.tsx
+++ src/components/MusicLibrarySetting/MusicLibrarySetting.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Dispatch } from "redux";
+import { connect, useDispatch } from "react-redux";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { SettingsState, updateSettings } from "../reducers/settings.reducer";
-import Button from "./Button";
-import Api from "../apiClient";
-import { TranslateFn } from "../i18n";
+import { SettingsState, updateSettings } from "../../reducers/settings.reducer";
+import Button from "../Button";
+import Api from "../../apiClient";
+import { TranslateFn } from "../../i18n";
 
 const Container = styled.div``;
 
@@ -28,14 +27,14 @@ const MusicLibrarySettingPath = styled.div`
 type MusicLibrarySettingProps = {
   musicLibraryPath?: string;
   t: TranslateFn;
-  dispatch: Dispatch;
 };
 
 const MusicLibrarySetting = ({
   musicLibraryPath,
   t,
-  dispatch,
 }: MusicLibrarySettingProps) => {
+  const dispatch = useDispatch();
+
   const removeLibraryPath = () => {
     dispatch(
       updateSettings({
@@ -62,21 +61,23 @@ const MusicLibrarySetting = ({
           onClick={removeLibraryPath}
           isSecondary
           disabled={!musicLibraryPath}
+          data-testid="MusicLibrarySettingRemoveButton"
         >
           <FontAwesomeIcon icon="trash" />
         </Button>
       </MusicLibrarySettingPath>
-      <Button onClick={addLibraryPath} isPrimary disabled={!!musicLibraryPath}>
+      <Button
+        onClick={addLibraryPath}
+        isPrimary
+        disabled={Boolean(musicLibraryPath)}
+      >
         {t("settings.library.addNew")}
       </Button>
     </Container>
   );
 };
 
-export default connect(
-  (state: { settings: SettingsState }) => ({
-    musicLibraryPath: state.settings.musicLibraryPath,
-    t: state.settings.t,
-  }),
-  (dispatch) => ({ dispatch }),
-)(MusicLibrarySetting);
+export default connect((state: { settings: SettingsState }) => ({
+  musicLibraryPath: state.settings.musicLibraryPath,
+  t: state.settings.t,
+}))(MusicLibrarySetting);
