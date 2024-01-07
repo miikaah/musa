4d41899f0d4a499d99132a65e9cf7d2cb02db148
Miika Henttonen <miika.henttonen@kipina.fi>
2024-01-07 15:49:31 +0200

    Add a heartbeat monitor

--- src/index.tsx
+++ src/index.tsx
@@ -9,18 +9,45 @@ import { SettingsState } from "./reducers/settings.reducer";
 import { store } from "./store";
 
 import "./index.css";
+import { PlayerState } from "./reducers/player.reducer";
 
 let previousSettings: Partial<SettingsState>;
-let timerId: NodeJS.Timeout;
+let previousPlayer: Partial<PlayerState>;
+let insertSettingsTimerId: NodeJS.Timeout;
+let heartbeatMonitorTimerId: NodeJS.Timeout;
+
 store.subscribe(() => {
+  // TODO: Clean this up
+  // @ts-ignore
+  const player = { ...store.getState().player } as Partial<PlayerState>;
+
+  // Turn on the heartbeat monitor when starting playing and close it in the opposite case
+  if (
+    origin.includes("fly.dev") &&
+    !previousPlayer.isPlaying &&
+    player.isPlaying
+  ) {
+    heartbeatMonitorTimerId = setInterval(async () => {
+      await fetch(`${window.origin}/heartbeat`);
+    }, 30_000);
+  } else if (
+    origin.includes("fly.dev") &&
+    previousPlayer.isPlaying &&
+    !player.isPlaying
+  ) {
+    clearInterval(heartbeatMonitorTimerId);
+  }
+
+  previousPlayer = player;
+
   const settings = { ...store.getState().settings } as Partial<SettingsState>;
   // Delete functions because they can not be serialized to JSON
-  delete settings.t;
+  delete settings.t; // NOTE: It's IMPORTANT this stays here so that not is equal check works correctly
 
   if (settings.isInit && !isEqual(previousSettings, settings)) {
-    clearTimeout(timerId);
+    clearTimeout(insertSettingsTimerId);
 
-    timerId = setTimeout(() => {
+    insertSettingsTimerId = setTimeout(() => {
       Api.insertSettings({
         ...settings,
         isInit: null,
