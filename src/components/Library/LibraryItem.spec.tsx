ea15f6f0c6c3ff325c10d7bee726f298d0985d0f
Miika Henttonen <miika.henttonen@gmail.com>
2024-08-07 13:14:38 +0300

    Update test

--- src/components/Library/LibraryItem.spec.tsx
+++ src/components/Library/LibraryItem.spec.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import LibraryItem from "./LibraryItem";
 import { albumFixture } from "../../fixtures/album.fixture";
 import { render } from "../../../test/render";
@@ -19,15 +18,5 @@ describe("LibraryItem", () => {
     render(<LibraryItem item={audio} />, {});
 
     expect(screen.getByText(title)).toBeInTheDocument();
-  });
-
-  it("double clicks and dispatches add to playlist action", async () => {
-    render(<LibraryItem item={albumFixture.files[0]} />, {});
-
-    await userEvent.dblClick(screen.getByText(title));
-
-    expect(mockDispatch).toHaveBeenCalledWith(
-      expect.objectContaining({ item: audio }),
-    );
   });
 });
